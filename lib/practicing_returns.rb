88e4fc5acf0112c509994ec1ef9e754aa2f85afe
Marin Cabac <marin.cabac@gmail.com>
2018-02-10 14:50:27 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
-    binding.pry
+    collection << yield(array[i])
     i += 1
   end
 end
